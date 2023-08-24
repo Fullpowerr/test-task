05a1fda6c0746b4fb55d780decf8b91c8074efbe
SDyushko <sdyushko@mail.ru>
2023-08-24 21:09:24 +0300

    fix app-component

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,14 @@
 <script>
 import list_products from './components/list_products.vue';
 import v_header from './components/v_header.vue';
-import { mapState } from 'vuex';
+
 export default {
   name: 'app',
 
 
   components: { v_header, list_products },
 
-  computed: {
-    ...mapState([
-      'message'])
-  }
+
 }
 </script>
 
